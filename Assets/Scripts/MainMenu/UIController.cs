01814ff4746356496e7d478c2a28aa10c0c3dec0
Dezrael <sergej31913@gmail.com>
2020-06-05 00:16:22 +0300

    Android Test

---
+++ Assets/Scripts/MainMenu/UIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+    public void AppExit()
+    {
+        Application.Quit();
+    }
+}
